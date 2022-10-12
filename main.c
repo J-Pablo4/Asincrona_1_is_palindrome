b7180f4e92c8e7ae2b2b99f2744aee0e577b03c5
Juan Pablo <pablo.lopez@iteso.mx>
2022-10-12 01:01:09 -0500

    initial version

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char *argv[]) {
+
+    if (!strcmp(argv[1], "is_palindrome"))
+        printf("Hurray_is_palindrome");
+
     return 0;
 }
